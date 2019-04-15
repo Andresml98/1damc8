8bbf5673a42aa9e54d57fa90aaab6f2c84a9a6ba
Andrés Martínez <Andresml98>
2019-04-15 08:58:21 +0200

    Version GIT 2

--- EjemploGIT2/src/Inicio.java
+++ EjemploGIT2/src/Inicio.java
@@ -4,6 +4,7 @@ public class Inicio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo Git");
+		System.out.println("Hola mundo Git2");
 	}
 
 }
